ff1301654502c4cd2b9114fb8eb14b4b16303d6d
SaikatNoman12 <noommaa240@gmail.com>
2022-06-15 00:32:27 +0600

    home page new section design and responsive

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -32,12 +32,36 @@ export class HomeComponent implements OnInit {
 
 
   popularBrandsName: any = [
-    { _id: 1, name: 'Renault KWID', img: 'https://stimg.cardekho.com/pwa/img/brandLogo_168x84/maruti.jpg' },
-    { _id: 2, name: 'Renault KWID', img: 'https://stimg.cardekho.com/pwa/img/brandLogo_168x84/hyundai.jpg' },
-    { _id: 3, name: 'Renault KWID', img: 'https://stimg.cardekho.com/pwa/img/brandLogo_168x84/tata.jpg' },
-    { _id: 4, name: 'Renault KWID', img: 'https://stimg.cardekho.com/pwa/img/brandLogo_168x84/mahindra.jpg' },
-    { _id: 5, name: 'Renault KWID', img: 'https://stimg.cardekho.com/pwa/img/brandLogo_168x84/kia.jpg' },
-    { _id: 6, name: 'Renault KWID', img: 'https://stimg.cardekho.com/pwa/img/brandLogo_168x84/mercedes-benz.jpg' },
+    {
+      _id: 1,
+      name: 'Maruti',
+      img: 'https://stimg.cardekho.com/pwa/img/brandLogo_168x84/maruti.jpg'
+    },
+    {
+      _id: 2,
+      name: 'Hyundai',
+      img: 'https://stimg.cardekho.com/pwa/img/brandLogo_168x84/hyundai.jpg'
+    },
+    {
+      _id: 3,
+      name: 'Tata',
+      img: 'https://stimg.cardekho.com/pwa/img/brandLogo_168x84/tata.jpg'
+    },
+    {
+      _id: 4,
+      name: 'Mahindra',
+      img: 'https://stimg.cardekho.com/pwa/img/brandLogo_168x84/mahindra.jpg'
+    },
+    {
+      _id: 5,
+      name: 'Kia',
+      img: 'https://stimg.cardekho.com/pwa/img/brandLogo_168x84/kia.jpg'
+    },
+    {
+      _id: 6,
+      name: 'Mercedes-Benz',
+      img: 'https://stimg.cardekho.com/pwa/img/brandLogo_168x84/mercedes-benz.jpg'
+    },
   ];
 
 
@@ -47,7 +71,7 @@ export class HomeComponent implements OnInit {
       name: 'Toyota Land Cruiser',
       price: '1.50 Cr',
       offer: 'Alert Me When Launched',
-      launch:'Aug 16, 2022 Expected Launch',
+      launch: 'Aug 16, 2022 Expected Launch',
       img: 'https://stimg.cardekho.com/images/carexteriorimages/630x420/Toyota/Land-Cruiser/8448/1623313668941/front-left-side-47.jpg'
     },
     {
@@ -55,16 +79,15 @@ export class HomeComponent implements OnInit {
       name: 'Hyundai Nexo',
       price: '65.00 Lakh',
       offer: 'Alert Me When Launched',
-      launch:'Mar 01, 2023 Expected Launch',
+      launch: 'Mar 01, 2023 Expected Launch',
       img: 'https://stimg.cardekho.com/images/carexteriorimages/630x420/Hyundai/Nexo/7255/1580972415014/front-left-side-47.jpg'
-
     },
     {
       _id: 3,
       name: 'Citroen C3',
       price: '65.00 Lakh',
       offer: 'Alert Me When Launched',
-      launch:'Aug 01, 2022 Expected Launch',
+      launch: 'Aug 01, 2022 Expected Launch',
       img: 'https://stimg.cardekho.com/images/carexteriorimages/630x420/Citroen/C3/8434/1631795589577/front-left-side-47.jpg'
     },
     {
@@ -72,10 +95,49 @@ export class HomeComponent implements OnInit {
       name: 'Tesla Model X',
       price: '2.00 Cr',
       offer: 'Alert Me When Launched',
-      launch:'Jul 10, 2022 Expected Launch',
+      launch: 'Jul 10, 2022 Expected Launch',
       img: 'https://stimg.cardekho.com/images/carexteriorimages/630x420/Tesla/Model-X/5253/1611841733029/front-left-side-47.jpg'
     },
-  ];;
+  ];
+
+  trustedUsedCars:any = [
+    {
+      _id: 1,
+      name: '2016 Maruti Baleno 2015-2022',
+      price: '85 Lakh',
+      fuelType:'Petrol',
+      kms:'23,358 Kms',
+      city:'Gurgaon',
+      img: 'https://images10.gaadicdn.com/usedcar_image/largephoto/VCC_1514799_24_1654950220.jpg'
+    },
+    {
+      _id: 2,
+      name: '2018 Hyundai Grand i10',
+      price: '4.72 Lakh',
+      fuelType:'Petrol',
+      kms:'35,277 Kms',
+      city:'Gurgaon',
+      img: 'https://images10.gaadicdn.com/usedcar_image/largephoto/VCC_1514466_24_1654944595.jpg'
+    },
+    {
+      _id: 3,
+      name: '2017 Maruti Baleno 2015-2022',
+      price: '5.01 Lakh',
+      fuelType:'Petrol',
+      kms:'24,026 Kms',
+      city:'New Delhi',
+      img: 'https://images10.gaadicdn.com/usedcar_image/largephoto/VCC_1510198_24_1654682419.jpg'
+    },
+    {
+      _id: 4,
+      name: '2014 Maruti Swift 2011-2014',
+      price: '3.31 Lakh',
+      fuelType:'Diesel',
+      kms:'1,14,345 Kms',
+      city:'New Delhi',
+      img: 'https://images10.gaadicdn.com/usedcar_image/largephoto/3583_1646454963_78_vcc_image.jpg'
+    },
+  ];
 
   constructor() { }
 
